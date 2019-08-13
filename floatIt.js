5fd2c4bf3e0d4f82c26bb469b93741dfe116543f
xjr7670 <xjr30226@126.com>
2019-08-13 22:57:46 +0800

    remove useless code

--- floatIt.js
+++ floatIt.js
@@ -43,21 +43,3 @@ cnt.onmouseover = function() {
 cnt.onmouseout = function() {
     itl = setInterval("floatDiv()", delay);
 };
-
-
-function IsPC() {  
-    var userAgentInfo = navigator.userAgent;
-    var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");  
-    var flag = true;  
-    for (var v = 0; v < Agents.length; v++) {  
-        if (userAgentInfo.indexOf(Agents[v]) > 0) { 
-            flag = false; 
-            break; 
-        }  
-    }  
-    return flag;  
-}
-
-if (IsPC() == false) {
-    document.querySelector('#main').style.display = 'none';
-}
